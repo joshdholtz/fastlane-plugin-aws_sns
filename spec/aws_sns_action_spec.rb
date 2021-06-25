093529ccd2e5e8965b9c32686565a7a5790c8a04
Marc PEREZ <marc.perez@epitech.eu>
2021-06-25 22:56:05 +0200

    Added a smoke test + updated CircleCI + updated Rubocop
    My CI/CD pipeline was sending me emails about test failures, so I updated them. To run the test, use `bundle exec rake` or `circleci local execute --job test`.
    The Gemfile.lock is committed to create a reproducible test environment. The .rubocop.yaml config was super outdated, so I did a quick namespace update.
    And I added a smoke test with a stub client. The code for passing the AWS SNS client is in another PR.

--- spec/aws_sns_action_spec.rb
+++ spec/aws_sns_action_spec.rb
@@ -1,9 +1,18 @@
 describe Fastlane::Actions::AwsSnsAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The aws_sns plugin is working!")
+    it 'doesn\'t smoke when you run it' do
+      expect(Fastlane::UI).to receive(:important).with('Created String')
 
-      Fastlane::Actions::AwsSnsAction.run(nil)
+      aws_sns_client = Aws::SNS::Client.new(stub_responses: true)
+
+      arn = Fastlane::Actions::AwsSnsAction.run({
+        aws_sns_client: aws_sns_client,
+        platform: 'FCM',
+        platform_name: 'FCM_SNS_PLATFORM_APP',
+        platform_fcm_server_key: 'PLACEHOLDER_FCM_KEY',
+        attributes_override: {}
+      })
+      expect(arn).to eq('String')
     end
   end
 end
